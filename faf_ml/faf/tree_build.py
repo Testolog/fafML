7ad67989038ac3fd12fe3b5519e273589390862f
whoaim <robert.nad@daxx.com>
2020-06-19 02:01:07 +0300

    minor fix

--- faf_ml/faf/tree_build.py
+++ faf_ml/faf/tree_build.py
@@ -1,10 +1,15 @@
 import json
+from itertools import product
 
-from pandas import DataFrame
+from pandas import DataFrame, MultiIndex
 from tabulate import tabulate
+
+PROCESS_COLUMN = ["Economy", 'Categories', 'Defense', 'General', 'Intel', 'Wreckage']
 
 with open("../../data/test/XSL0105_unit.json") as f:
     data = json.load(f)
-df: DataFrame = DataFrame(data)
-print(df.info())
+mp_index = MultiIndex.from_tuples(product(["XSL0105"], PROCESS_COLUMN))
+df: DataFrame = DataFrame(data, columns=PROCESS_COLUMN, index=["XSL0105"])
+print(DataFrame(data, columns=PROCESS_COLUMN, index=mp_index))
+e = DataFrame.from_records(df['Economy'], index=["XSL0105"])
 print(tabulate(df, headers="keys", tablefmt='psql'))
